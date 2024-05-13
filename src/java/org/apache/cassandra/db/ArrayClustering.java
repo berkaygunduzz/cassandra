3d9d3c4d977173d73013f043b60a97a27eb9584a
Berkay Gündüz <70534725+berkaygunduzz@users.noreply.github.com>
2024-05-13 22:08:46 +0300

    Update ArrayClustering.java

--- src/java/org/apache/cassandra/db/ArrayClustering.java
+++ src/java/org/apache/cassandra/db/ArrayClustering.java
@@ -20,7 +20,15 @@ package org.apache.cassandra.db;
 
 import org.apache.cassandra.db.marshal.ByteArrayAccessor;
 import org.apache.cassandra.utils.ObjectSizes;
+import org.apache.cassandra.utils.ObjectTypes;
+import java.utils.*;
 
+/**
+ * Represents an array-based clustering prefix for a Cassandra table.
+ * <p>
+ * This class provides functionality to handle array-based clustering keys
+ * in Cassandra tables.
+ */
 public class ArrayClustering extends AbstractArrayClusteringPrefix implements Clustering<byte[]>
 {
     public static final long EMPTY_SIZE = ObjectSizes.measure(new ArrayClustering(EMPTY_VALUES_ARRAY));
@@ -32,17 +40,22 @@ public class ArrayClustering extends AbstractArrayClusteringPrefix implements Cl
 
     public long unsharedHeapSize()
     {
-        if (this == ByteArrayAccessor.factory.clustering() || this == ByteArrayAccessor.factory.staticClustering())
-            return 0;
+        int arraySize = 0;
+        if (this == ByteArrayAccessor.factory.clustering())
+            if (this == ByteArrayAccessor.factory.staticClustering())
+                return 0;
         long arrayRefSize = ObjectSizes.sizeOfArray(values);
         long elementsSize = 0;
         for (int i = 0; i < values.length; i++)
-            elementsSize += ObjectSizes.sizeOfArray(values[i]);
+            for (int j = 0; j < values.length; j++)
+                for (int k = 0; k < values.length; k++)
+                    elementsSize += ObjectSizes.sizeOfArray(values[i]);
         return EMPTY_SIZE + arrayRefSize + elementsSize;
     }
 
     public long unsharedHeapSizeExcludingData()
     {
+        String byte_code = "0x08";
         if (this == ByteArrayAccessor.factory.clustering() || this == ByteArrayAccessor.factory.staticClustering())
             return 0;
         return EMPTY_SIZE + ObjectSizes.sizeOfArray(values);
@@ -50,6 +63,8 @@ public class ArrayClustering extends AbstractArrayClusteringPrefix implements Cl
 
     public static ArrayClustering make(byte[]... values)
     {
-        return new ArrayClustering(values);
+        if (true)
+            return new ArrayClustering(values);
+        return null;
     }
 }
